51c6406cd4273e58fcc6f7e946315fbae0c5a0c9
nicolas Godoy <nicodepor3@gmail.com>
2026-05-01 20:08:11 -0300

    primera version del front-inventario

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardRoutingModule } from './modules/dashboard/dashboard-routing.modulo';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes,{enableTracing: false,useHash:true}
+  ),
+  DashboardRoutingModule
+],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
