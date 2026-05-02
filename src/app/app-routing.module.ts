232cbd5f6f8926a8f12bd9cb60e6c5624871dc53
nicolas Godoy <nicodepor3@gmail.com>
2026-05-02 20:34:47 -0300

    sidenav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardRoutingModule } from './modules/dashboard/dashboard-routing.modulo';
+import { DashboardRoutingModule } from './modules/dashboard/dashboard-routing.module';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
