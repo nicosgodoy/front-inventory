72d98a3192ab22bfbc31bca82ddb906a1c9feb1c
nicolas Godoy <nicodepor3@gmail.com>
2026-05-04 00:40:25 -0300

    Agregando la tabla de categorias

--- src/app/modules/dashboard.module.ts
+++ src/app/modules/dashboard.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/pages/dashboard.component';
 import { HomeComponent } from './dashboard/components/home/home.component';
 import { SharedModule } from './shared/shared.module';
+import { CategoryModule } from './category/category.module';
 
 
 
@@ -13,7 +14,8 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    CategoryModule
   ]
 })
 export class DashboardModule { }
